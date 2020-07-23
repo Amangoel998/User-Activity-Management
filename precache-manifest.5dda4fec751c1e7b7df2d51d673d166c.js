76151a935edafc37a554076d7d628395e6b822de
Aman Goel <amangoel998@gmail.com>
2020-07-23 15:20:38 +0530

    Updates

---
+++ precache-manifest.5dda4fec751c1e7b7df2d51d673d166c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ae99502c8023ac2ad8621ab83dc64d",
+    "url": "/User-Activity-Management/index.html"
+  },
+  {
+    "revision": "d2379583b5df88fd385c",
+    "url": "/User-Activity-Management/static/css/2.701597db.chunk.css"
+  },
+  {
+    "revision": "9b33ade06a5d5668b43d",
+    "url": "/User-Activity-Management/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d2379583b5df88fd385c",
+    "url": "/User-Activity-Management/static/js/2.92bef366.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/User-Activity-Management/static/js/2.92bef366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b33ade06a5d5668b43d",
+    "url": "/User-Activity-Management/static/js/main.471bfac6.chunk.js"
+  },
+  {
+    "revision": "b92fa17dac5aa95d43c8",
+    "url": "/User-Activity-Management/static/js/runtime-main.f6d17a9c.js"
+  }
+]);
